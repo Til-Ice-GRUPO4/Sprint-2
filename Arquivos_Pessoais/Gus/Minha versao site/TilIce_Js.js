5b77dc9ecbd1c2c5c112890ee19388fe4538bbe2
GusKSP <gus.paulino@stpech.school>
2026-03-31 20:07:05 -0300

    Adicionando diretórios para arquivos pessoais do grupo e reestruturando versão do site institucional pessoal

--- Arquivos_Pessoais/Gus/Minha versao site/TilIce_Js.js
+++ Arquivos_Pessoais/Gus/Minha versao site/TilIce_Js.js
@@ -1,15 +1,15 @@
 function Travel_Home() {
-    window.location.href = "Til-Ice_Gus_Home.html"
+    location.href = "Til-Ice_Gus_Home.html"
 }
 
 function Travel_Grupo() {
-    window.location.href = "Til-Ice_Gus_Grupo.html"
+    location.href = "Til-Ice_Gus_Grupo.html"
 }
 
 function Travel_Sobre() {
-    window.location.href = "Til-Ice_Gus_Sobre.html"
+    location.href = "Til-Ice_Gus_Sobre.html"
 }
 
 function Travel_Localizacao() {
-    window.location.href = "Til-Ice_Gus_Localizacao.html"
+    location.href = "Til-Ice_Gus_Localizacao.html"
 }
