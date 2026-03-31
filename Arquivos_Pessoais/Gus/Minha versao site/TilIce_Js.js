be669df5eceb5ded66c191daee30774b9a0f4c37
GusKSP <gus.paulino@stpech.school>
2026-03-30 21:07:19 -0300

    Adicionando arquivo para projetos pessoais relacionados ao trabalho de PI. Junto de projeto pessoal com versao do site institucional

---
+++ Arquivos_Pessoais/Gus/Minha versao site/TilIce_Js.js
@@ -0,0 +1,15 @@
+function Travel_Home() {
+    window.location.href = "Til-Ice_Gus_Home.html"
+}
+
+function Travel_Grupo() {
+    window.location.href = "Til-Ice_Gus_Grupo.html"
+}
+
+function Travel_Sobre() {
+    window.location.href = "Til-Ice_Gus_Sobre.html"
+}
+
+function Travel_Localizacao() {
+    window.location.href = "Til-Ice_Gus_Localizacao.html"
+}
